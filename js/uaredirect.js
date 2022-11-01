615794344c7bce924c6e23a69858256681e8da51
webCN9955 <117191770+webCN9955@users.noreply.github.com>
2022-11-01 23:50:33 +0800

    ceshi

---
+++ js/uaredirect.js
@@ -0,0 +1,15 @@
+//判断是否移动端，如果是则跳转到指定的URL地址
+function browserRedirect(url) {
+    //只读的字符串，声明了浏览器用于 HTTP 请求的用户代理头的值
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+    var bIsAndroid = sUserAgent.match(/android/i) == "android";
+    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+    if (bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+    window.location.replace(url);
+    }
+}
